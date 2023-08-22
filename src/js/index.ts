50fac05087b65b25776f5ec83493e22497a4aebf
Yurii Vlasiuk <yuriivlasiuk@gmail.com>
2023-08-22 14:47:05 +0300

    update: mionimal implementation with getting files list recursively

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,3 +1,27 @@
+import * as fs from "fs";
+import * as path from "path";
+
 export function greet(name: string): string {
     return `Hello ${name}!`;
 }
+
+function getAllFilePaths(directoryPath, filePaths = []) {
+    const files = fs.readdirSync(directoryPath);
+
+    for (const file of files) {
+        const filePath = path.join(directoryPath, file);
+        const stats = fs.statSync(filePath);
+
+        if (stats.isFile()) {
+            filePaths.push(filePath);
+        } else if (stats.isDirectory()) {
+            getAllFilePaths(filePath, filePaths);
+        }
+    }
+
+    return filePaths;
+}
+
+const pathes = getAllFilePaths(path.join(__dirname, "..", "..", "src", "assets"));
+
+console.log(pathes);
