7d93d4824cbc535fe8afea1b2c10809b16c87278
Yurii Vlasiuk <yuriivlasiuk@gmail.com>
2023-08-28 13:13:27 +0300

    update: add test for parseRegexYamls

--- tests/js/parse.test.ts
+++ tests/js/parse.test.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import * as path from "path";
 
-import { getAllFilePaths } from "../../src/js";
+import { getAllFilePaths, parseRegexYamls } from "../../src/js";
 
 const assetsPaths = [
     "file/applications/espresso/5.4.1/pw.x/stdin.yml",
@@ -21,5 +21,27 @@ describe("parse tests", () => {
         expect(allPaths.length).to.be.eql(9);
 
         allPaths.forEach((assetPath, index) => expect(assetPath).to.contain(assetsPaths[index]));
+    });
+
+    it("should parse Regex YAML", () => {
+        const filePath = path.join(
+            __dirname,
+            "..",
+            "assets",
+            "file/applications/espresso/5.4.1/pw.x/stdin.yml",
+        );
+        const regexObject = parseRegexYamls(filePath);
+
+        expect(regexObject.filePath).to.be.eql(filePath);
+        expect(regexObject.parsedContent).to.be.eql({
+            _fingerprints: [
+                { regex: "^&control", flags: ["g", "i"], isRequired: true },
+                { regex: "^&electrons", flags: ["g", "i"], isRequired: true },
+            ],
+            control: {
+                _format: { calculation: { regex: "...", flags: ["g", "m", "i"] } },
+                calculation: { regex: "...", flags: ["g", "m", "i"] },
+            },
+        });
     });
 });
