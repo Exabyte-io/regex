6c0ea753cd02f76b3bc0dd17a983a922135bd3c6
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2026-06-13 15:40:31 +0800

    use more strict regex for optional constraints

--- tests/js/functions.test.ts
+++ tests/js/functions.test.ts
@@ -34,7 +34,7 @@ const REFERENCE_YAML_CONTENT = {
             params: {
                 UNIT: ["alat", "bohr", "angstrom", "crystal", "crystal_sg"],
             },
-            regex: "ATOMIC_POSITIONS\\s*[{(]?\\s*(alat|bohr|angstrom|crystal|crystal_sg)?\\s*[)}]?\\s*\\n((?:[ \\t]*\\w+[ \\t]+[-+]?(?:\\d+\\.\\d*|\\.\\d+|\\d+)(?:[eEdD][-+]?\\d+)?[ \\t]+[-+]?(?:\\d+\\.\\d*|\\.\\d+|\\d+)(?:[eEdD][-+]?\\d+)?[ \\t]+[-+]?(?:\\d+\\.\\d*|\\.\\d+|\\d+)(?:[eEdD][-+]?\\d+)?.*\\n?)+)",
+            regex: "ATOMIC_POSITIONS\\s*[{(]?\\s*(alat|bohr|angstrom|crystal|crystal_sg)?\\s*[)}]?\\s*\\n((?:[ \\t]*\\w+(?:[ \\t]+[-+]?(?:\\d+\\.\\d*|\\.\\d+|\\d+)(?:[eEdD][-+]?\\d+)?){3}(?:(?:[ \\t]+[01]){3})?[ \\t]*\\n?)+)",
         },
         cell_parameters_card: {
             flags: ["i"],
