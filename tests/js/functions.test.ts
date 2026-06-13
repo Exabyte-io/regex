273d825e2ae0699c6a29fac691e639c6a6c90e36
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2026-06-13 13:25:57 +0800

    add regex for atomic positions card

--- tests/js/functions.test.ts
+++ tests/js/functions.test.ts
@@ -29,6 +29,13 @@ const REFERENCE_YAML_CONTENT = {
         { regex: "^&electrons", flags: ["g", "i"], isRequired: true },
     ],
     _regex_dict: {
+        atomic_positions_card: {
+            flags: ["i"],
+            params: {
+                UNIT: ["alat", "bohr", "angstrom", "crystal", "crystal_sg"],
+            },
+            regex: "ATOMIC_POSITIONS\\s*[{(]?\\s*(alat|bohr|angstrom|crystal|crystal_sg)?\\s*[)}]?\\s*\\n((?:[ \\t]*\\w+[ \\t]+[-+]?(?:\\d+\\.\\d*|\\.\\d+|\\d+)(?:[eEdD][-+]?\\d+)?[ \\t]+[-+]?(?:\\d+\\.\\d*|\\.\\d+|\\d+)(?:[eEdD][-+]?\\d+)?[ \\t]+[-+]?(?:\\d+\\.\\d*|\\.\\d+|\\d+)(?:[eEdD][-+]?\\d+)?.*\\n?)+)",
+        },
         cell_parameters_card: {
             flags: ["i"],
             params: { UNIT: ["alat", "bohr", "angstrom"] },
@@ -104,7 +111,15 @@ describe("build schema from assets tests", () => {
     it("should load Regex YAML and interpolate primitives and params", () => {
         const regexObject = loadRegexYAMLs(REFERENCE_PATH_TO_PWIN_YML);
 
-        const primitivesPath = path.join(__dirname, "..", "..", "src", "assets", "file", "primitives.yml");
+        const primitivesPath = path.join(
+            __dirname,
+            "..",
+            "..",
+            "src",
+            "assets",
+            "file",
+            "primitives.yml",
+        );
         const primitivesObject = loadRegexYAMLs(primitivesPath);
 
         let interpolatedContent = interpolatePrimitives(
